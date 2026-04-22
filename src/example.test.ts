556ebe358cfb0d9b952639c01f2be5f10321866e
thaivlinh <thaivlinh1995@gmail.com>
2026-04-22 19:17:03 +0700

    Add: test

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,35 +1,54 @@
+import { defineEntity, MikroORM, p } from '@mikro-orm/postgresql';
 import 'reflect-metadata';
-import { Entity, PrimaryKey, Property, ReflectMetadataProvider } from '@mikro-orm/decorators/legacy';
-import { MikroORM } from '@mikro-orm/sqlite';
 
-@Entity()
-class User {
-
-  @PrimaryKey()
-  id!: number;
-
-  @Property()
-  name: string;
-
-  @Property({ unique: true })
-  email: string;
-
-  constructor(name: string, email: string) {
-    this.name = name;
-    this.email = email;
+export const ItemSchema = defineEntity({
+  name: 'Item',
+  indexes: [
+    {
+      name: 'item_deleted_at_idx',
+      expression: (columns, table) =>
+        `CREATE INDEX item_deleted_at_idx ON ${table.qualifiedName} (${columns.code}) WHERE ${columns.deletedAt} IS NULL`,
+    },
+  ],
+  properties: {
+    id: p.integer().primary(),
+    code: p.text(),
+    deletedAt: p.datetime().nullable()
   }
+})
+export class Item extends ItemSchema.class {}
+ItemSchema.setClass(Item);
 
-}
+export const UserSchema = defineEntity({
+  name: 'User',
+  schema: 'auth',
+  indexes: [
+    {
+      name: 'user_deleted_at_idx',
+      expression: (columns, table) =>
+        `CREATE INDEX user_deleted_at_idx ON ${table.qualifiedName} (${columns.code}) WHERE ${columns.deletedAt} IS NULL`,
+    },
+  ],
+  properties: {
+    id: p.integer().primary(),
+    code: p.text(),
+    deletedAt: p.datetime().nullable()
+  }
+})
+export class User extends UserSchema.class {}
+UserSchema.setClass(User);
 
 let orm: MikroORM;
 
 beforeAll(async () => {
   orm = await MikroORM.init({
-    dbName: ':memory:',
-    entities: [User],
-    metadataProvider: ReflectMetadataProvider,
+    clientUrl: 'postgresql://neondb_owner:npg_TsDK7vCqH1ib@ep-odd-firefly-a1rh5omt-pooler.ap-southeast-1.aws.neon.tech/mikro-orm-reproduction?sslmode=require&channel_binding=require',
+    entities: [Item, User],
     debug: ['query', 'query-params'],
     allowGlobalContext: true, // only for testing
+    driverOptions: {
+      ssl: { rejectUnauthorized: false },
+    },
   });
   await orm.schema.refresh();
 });
@@ -38,17 +57,21 @@ afterAll(async () => {
   await orm.close(true);
 });
 
-test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
-  await orm.em.flush();
-  orm.em.clear();
+test('item - custom index changes on public schema', async () => {
+  const meta = orm.getMetadata().get(Item);
+  meta.indexes = []
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
-  await orm.em.flush();
+  const updateSchema = await orm.schema.getUpdateSchemaSQL();
+  console.log(updateSchema, 'Update schema for Item');
+  await orm.schema.update()
+});
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+
+test('user - custom index changes on non-public schema', async () => {
+  const meta = orm.getMetadata().get(User);
+  meta.indexes = []
+
+  const updateSchema = await orm.schema.getUpdateSchemaSQL();
+  console.log(updateSchema, 'Update schema for User');
+  await orm.schema.update()
 });
